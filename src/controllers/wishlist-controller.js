3134c8fff64030adf95f7d28cec52990520d1703
Mustafa Mahmoud <mustafaelnoty@gmail.com>
2026-09-02 18:34:22 +0300

    add documentation comment

--- src/controllers/wishlist-controller.js
+++ src/controllers/wishlist-controller.js
@@ -2,12 +2,22 @@ import asyncHandler from "express-async-handler";
 
 import * as wishlistService from "../services/database/wishlist-service.js";
 
+/**
+ *@desc        Get user wishlist
+ *@route       GET /api/v1/wishlist
+ *@access      protect
+ */
 export const getWishlist = asyncHandler(async (req, res, next) => {
   const wishlist = await wishlistService.getWishlist(req.user._id);
 
   res.status(200).json({ status: "success", data: wishlist });
 });
 
+/**
+ *@desc        Add product to wishlist
+ *@route       POST /api/v1/wishlist
+ *@access      protect
+ */
 export const addProduct = asyncHandler(async (req, res, next) => {
   const wishlist = await wishlistService.addProduct(
     req.user._id,
@@ -17,6 +27,11 @@ export const addProduct = asyncHandler(async (req, res, next) => {
   res.status(200).json({ status: "success", data: wishlist });
 });
 
+/**
+ *@desc        Remove product from wishlist
+ *@route       DELETE /api/v1/wishlist
+ *@access      protect
+ */
 export const removeProduct = asyncHandler(async (req, res, next) => {
   await wishlistService.removeProduct(req.user._id, req.body.productId);
 
