7d2544f7ac146f5a9cf4269f6d1a31eac8d46f02
Mustafa Mahmoud <mustafaelnoty@gmail.com>
2026-09-07 23:46:36 +0300

    Fix the handling of empty keys

--- src/middlewares/validator-middleware.js
+++ src/middlewares/validator-middleware.js
@@ -5,9 +5,12 @@ const validatorMiddleware = (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     const errorMessages = errors.array().reduce((acc, err) => {
-      if (!acc[err.path]) {
+      if (err.path && !acc[err.path]) {
         acc[err.path] = err.msg;
+      } else if (!err.path) {
+        acc.general = err.msg;
       }
+
       return acc;
     }, {});
     return next(new ApiError(errorMessages, 400));
