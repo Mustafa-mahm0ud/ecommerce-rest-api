9316170d1f5bb1a1e60175c4547964e8c169e844
Mustafa Mahmoud <mustafaelnoty@gmail.com>
2026-09-11 04:33:09 +0300

    style: comment

--- src/services/database/address-service.js
+++ src/services/database/address-service.js
@@ -63,7 +63,7 @@ export const removeAddress = async (userId, addressId) => {
 
   if (!doc) throw new ApiError(`No address found with id: ${addressId}`, 404);
 
-  // just keep it To provide query
+  // Just keep it to provide query
   if (doc.addresses[0].isDefault)
     throw new ApiError(
       "Please add a default address before clearing the current default address",
